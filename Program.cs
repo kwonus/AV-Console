ddee08cc8d511720dc172102dc83e8d88ef3c833
Kevin Wonus <kevin@wonus.com>
2024-01-04 23:36:08 -0500

    iRemoved cruft

--- Program.cs
+++ Program.cs
@@ -38,67 +38,6 @@ namespace AVConsole
                     {
                         Console.Error.WriteLine(tuple.message);
                     }
-                    if (tuple.stmt != null)
-                    {
-                        //singletons.ProcessStatement(tuple.stmt, tuple.result); // process singletons and persistent settings
-                        /*
-                        if (tuple.stmt.Commands != null)
-                        {
-                            int cnt = 0;
-                            foreach (var segment in tuple.stmt.Commands.Searches)
-                            {
-                                if (++cnt == 1)
-                                    Console.Error.WriteLine("SEARCH SEGMENTS:");
-
-                                foreach (var line in segment.AsYaml())
-                                {
-                                    Console.WriteLine(line);
-                                }
-                            }
-                            cnt = 0;
-                            foreach (var segment in tuple.stmt.Commands.Assignments)
-                            {
-                                if (++cnt == 1)
-                                    Console.Error.WriteLine("ASSIGNMENTS:");
-
-                                foreach (var line in segment.AsYaml())
-                                {
-                                    Console.WriteLine(line);
-                                }
-                            }
-                            cnt = 0;
-                            foreach (var segment in tuple.stmt.Commands.Filters)
-                            {
-                                if (++cnt == 1)
-                                    Console.Error.WriteLine("FILTERS:");
-
-                                foreach (var line in segment.AsYaml())
-                                {
-                                    Console.WriteLine(line);
-                                }
-                            }
-                            if (tuple.stmt.Commands.Display != null)
-                            {
-                                Console.Error.WriteLine("DISPLAY:");
-
-                                foreach (var line in tuple.stmt.Commands.Display.AsYaml())
-                                {
-                                    Console.WriteLine(line);
-                                }
-                            }
-                            if (tuple.stmt.Commands.Export != null)
-                            {
-                                Console.Error.WriteLine("EXPORT:");
-
-                                foreach (var line in tuple.stmt.Commands.Export.AsYaml())
-                                {
-                                    Console.WriteLine(line);
-                                }
-                            }
-                            var stmt = new NativeStatement(tuple.stmt.Blueprint);
-                        
-                        }*/
-                    }
                 }
                 else
                 {
