5e6fb9bcbe1ac02978f4d5754ba69b49401b26d1
Kevin Wonus <kevin@wonus.com>
2024-01-23 23:55:51 -0500

    First attempt at showing results of search in AV-Console.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
-﻿using AVXFramework;
+﻿using AVSearch.Model.Expressions;
+using AVSearch.Model.Results;
+using AVXFramework;
+using AVXLib.Memory;
 using Blueprint.Blue;
+using Blueprint.Model.Implicit;
+using System.Data;
 using System.Text.Json;
+using static Blueprint.Model.Implicit.QFormat;
 
 namespace AVConsole
 {
@@ -42,6 +48,48 @@ namespace AVConsole
                     if (error)
                     {
                         Console.Error.WriteLine("One or more errors were encountered");
+                    }
+                    else if (tuple.find != null && tuple.find.Expressions != null)
+                    {
+                        QueryResult result = tuple.find;
+
+                        foreach (SearchExpression exp in result.Expressions)
+                        {
+                            if (exp.Hits > 0)
+                            {
+                                foreach (QueryBook book in exp.Books.Values)
+                                {
+                                    byte v = 0;
+                                    foreach (QueryChapter chapter in book.Chapters.Values)
+                                    {
+                                        Dictionary<BCVW, QueryTag> tags = new(); //<verse, tag>
+                                        foreach (QueryMatch match in chapter.Matches)
+                                        {
+                                            if (v != match.Start.V)
+                                            {
+                                                if (tags.Count > 0)
+                                                {
+                                                    engine.Render(Console.Out, book.BookNum, chapter.ChapterNum, v, QFormatVal.MD, QLexicalDisplay.QDisplayVal.AV, tags);
+                                                    tags.Clear();
+                                                }
+                                                v = match.Start.V;
+                                            }
+                                            foreach (QueryTag highlight in match.Highlights)
+                                            {
+                                                tags[highlight.Coordinates] = highlight;
+                                            }
+                                        }
+                                        if (tags.Count > 0)
+                                        {
+                                            engine.Render(Console.Out, book.BookNum, chapter.ChapterNum, v, QFormatVal.MD, QLexicalDisplay.QDisplayVal.AV, tags);
+                                            tags.Clear();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+
                     }
                 }
                 else
